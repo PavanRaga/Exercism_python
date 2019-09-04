bcc65cb0facad39930add73cecd4771206725465
PavanRaga <pavan.raga98@gmail.com>
2019-09-04 19:09:07 +0800

    second

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,2 +1,20 @@
+import re
+
+with open("README.md","r") as f:
+    file_content = f.read()
+
+# Get the text from file
+m = re.search('```text([\n\w\s:.,-]+)',file_content)
+string = m.group(1)
+
+#split them to form verse
+new_string = list(filter(len, string.splitlines()))
+len_text = len(new_string)
+
 def recite(start_verse, end_verse):
-    pass
+    if start_verse > len_text or end_verse > len_text or end_verse < start_verse:
+        print("Invalid request")
+    if start_verse == end_verse:
+        return [(new_string[start_verse-1])]
+    else:
+        return new_string[start_verse-1:end_verse]
